eebd532ebbf13a95e5f53011908651943150a2a3
oscar <oscar.burman005@gmail.com>
2025-05-19 13:48:51 +0200

    Set up the product router

--- routes/ApiRouter.js
+++ routes/ApiRouter.js
@@ -1,5 +1,8 @@
 import express from "express";
+import ProductRouter from "./ProductRouter.js";
 
 const router = express.Router();
+
+router.use("/products", ProductRouter);
 
 export default router;
---
+++ routes/ProductRouter.js
@@ -0,0 +1,20 @@
+import expres from "express";
+
+const router = expres.Router();
+
+//get product by id
+// router.get("/:productId");
+
+//get all products, optionally include query params like sort, order, searchquery and limit
+// router.get("/");
+
+//add a new product - needs to be proected so only admins can add a new product
+// router.post("/");
+
+//update the name, price and more of an existing product
+// router.patch("/:productId");
+
+//delete a product
+// router.delete("/:productId");
+
+export default router;
