4376781dcd1dc01634fb9e5de068913e8b4c6cce
Burtarn <robin.andersson0125@gmail.com>
2025-05-19 14:07:40 +0200

    validators

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,12 +1,12 @@
 import jwt from 'jsonwebtoken';
 
 export const generateToken = (payload) => {
-  const secret = process.env.JWT_SECRET;
-  if (!secret) {
+     const secret = process.env.JWT_SECRET;
+     if (!secret) {
     throw new Error("JWT_SECRET is not defined");
-  }
+    }
 
-  return jwt.sign(payload, secret, { expiresIn: '1h' });
+    return jwt.sign(payload, secret, { expiresIn: '1h' });
 };
 
 export const verifyToken = (token) => {
--- validators/authValidator.js
+++ validators/authValidator.js
@@ -1,1 +1,8 @@
-//validate auth requests
+import { z } from "zod";
+
+const loginSchema = z.object({
+    username: z.string().min(1, "username is requiered"),
+    password: z.string().min(6, "Password must be at least 6 chars"),
+
+});
+
