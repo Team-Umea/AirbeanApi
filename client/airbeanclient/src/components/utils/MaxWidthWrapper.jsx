bc3ac32f21af6a2097bb75a5ed5a13bb721125c1
Tobias Engvall <tobias.engvall@chasacademy.se>
2025-05-21 16:06:19 +0200

    added bg-amber-100 to layout-wrapper

--- client/airbeanclient/src/components/utils/MaxWidthWrapper.jsx
+++ client/airbeanclient/src/components/utils/MaxWidthWrapper.jsx
@@ -1,10 +1,20 @@
 import { cn } from "../../lib/utitls";
 
-const MaxWidthWrapper = ({ classNameWrapper, classNameContainer, children }) => {
+const MaxWidthWrapper = ({
+  classNameWrapper,
+  classNameContainer,
+  children,
+}) => {
   return (
     <div
-      className={cn("mx-auto w-full px-2.5 md:px-20 flex flex-col items-center", classNameWrapper)}>
-      <div className={cn("w-full max-w-screen-xl", classNameContainer)}>{children}</div>
+      className={cn(
+        "mx-auto w-full px-2.5 md:px-20 flex flex-col items-center",
+        classNameWrapper
+      )}
+    >
+      <div className={cn("w-full max-w-screen-xl", classNameContainer)}>
+        {children}
+      </div>
     </div>
   );
 };
