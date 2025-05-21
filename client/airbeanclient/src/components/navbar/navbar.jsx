3abe7062ea9f2887c8641e3dcbc09a6f05665ebd
oscar <oscar.burman005@gmail.com>
2025-05-21 11:30:50 +0200

    Commit for merge

--- client/airbeanclient/src/components/navbar/navbar.jsx
+++ client/airbeanclient/src/components/navbar/navbar.jsx
@@ -1,8 +1,13 @@
 import { Link } from "react-router-dom";
 import "../../components/navbar/navbar.css";
 import { useSelector } from "react-redux";
+import { FaShoppingCart } from "react-icons/fa";
 
 export default function Navbar() {
+  const cartItemCount = 2; // redux-lösning för denna sen.
+
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
@@ -12,8 +17,8 @@ export default function Navbar() {
           <Link to="/about">Vårt kaffe</Link>
           <Link to="/profil">Min profil</Link>
           <Link to="/orderstatus">Orderstatus</Link>
-          <Link to="/register">Skapa konto</Link>
-          <Link to="/login">Logga in</Link>
+          {isAuthenticated && <Link to="/register">Skapa konto</Link>}
+          {isAuthenticated && <Link to="/login">Logga in</Link>}
         </div>
       </div>
     </nav>
